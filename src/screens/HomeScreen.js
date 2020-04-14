3ce89387a8bb6ed78949a1fa26613f41c879249b
CarlKarlQarl <karlmatthes@gmail.com>
2020-04-14 15:43:13 -0600

    Complete through Section 5-29

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,26 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from "react"
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native"
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello, World!</Text>;
-};
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello, World!</Text>
+      <Button 
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")} 
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
   }
-});
+})
 
-export default HomeScreen;
+export default HomeScreen
